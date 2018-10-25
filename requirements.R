804b17833f759c50b128f43319177470c1477931
Grant Fiddyment <gfiddyment@sixers.com>
2018-10-25 19:58:04 -0400

    W4 update

--- requirements.R
+++ requirements.R
@@ -1,3 +1,4 @@
+chooseCRANmirror()
 packages = c("rmarkdown", "car", "caret", "coefplot", "corrplot", "ctv", "DataCombine", "GGally", "gbm", "ggplot2", "glmnet", "HH", "Hmisc", "ISLR", "lattice", "lm", "lmtest", "leaps", "MASS", "perturb", "pls", "pROC", "psych", "randomForest", "ROCR", "tree", "VGAM", "gam", "akima", "e1071", "stargazer", "data.table", "XML", "xlsx", "xlsxjars", "rJava")
 
 for (package in packages) {
